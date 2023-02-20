3b56c2d7b8cca0f741ad1ce84c5c4fb51ae1f027
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-02-20 23:17:53 +0600

    courses data upload

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './Courses.css'
+import React, { useEffect, useState } from "react";
+import Card from "../../Components/Card/Card";
+import "./Courses.css";
 
 const Courses = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("CourseData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+  return (
+    <div className="row container m-auto">
+      <div className="col-4">
+        <h3>all categories</h3>
+        <ul className="list-style-none">
+          <li>Masters</li>
+          <li>Honours</li>
+          <li>Hsc</li>
+          <li>Ssc</li>
+          <li>Jsc</li>
+          <li>Psc</li>
+        </ul>
+      </div>
+      <div className="col-8">
+        {data.map((dt) => (
+          <Card dt={dt}></Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
